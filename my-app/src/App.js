3f0684d58c15a33401dd3e6f50a0fbba4bf3cb00
heloisaatss <heloisa_assoni@hotmail.com>
2022-07-06 21:56:06 -0300

    lab_01

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Opa! aula react!
+      <h1>Heloisa</h1>
+    </div>
+  );
+}
+
+export default App;
