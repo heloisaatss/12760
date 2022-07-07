1bac01efa6721cdd2f03753696ec500f6847cf47
heloisaatss <heloisa_assoni@hotmail.com>
2022-07-07 20:27:46 -0300

    lab 02

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,19 +6,40 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Ola(props) {
-  return <h1>Opa! {props.name} {props.sobrenome}</h1>
+
+// function BoasVindas(props) {
+//   return <Opa name={props.name}></Opa>
+// }
+
+class Chegada extends React.Component {
+  render() {
+    return <h1>Opa, {this.props.name}</h1>
+  }
 }
 
-const element = <Ola name='João' sobrenome='Souza'></Ola>
+class BoasVindas extends React.Component {
+  render() {
+    return <Chegada name={this.props.name}></Chegada>
+  }
+}
+
+function Opa(props) {
+  return <div>
+    <BoasVindas name={props.name}></BoasVindas>
+    <BoasVindas name={props.name}></BoasVindas>
+  </div>
+
+}
 
 root.render(
-  element,
-  document.getElementById('root')
-)
+  <div>
+    <Opa name='Pessoa 01'></Opa>
+    <Opa name='Pessoa 04'></Opa>
+  </div>
+);
 
 
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
