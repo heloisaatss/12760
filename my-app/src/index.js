d43de3af599c0d7ab0030425fd3a70a82f8e95af
heloisaatss <heloisa_assoni@hotmail.com>
2022-07-07 21:45:39 -0300

    lab 04 exercício

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,45 +6,52 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-class Pesquisa extends React.Component {
+class Cadastro extends React.Component {
   render() {
-    return <h1>Componente de pesquisa</h1>
+    return <h1>Cadastro de Usuário</h1>
   }
 }
 
-class ListaProdutos extends React.Component {
+class DadosBasicos extends React.Component {
   render() {
     return <div>
-      <h2>Lista de resultado</h2>
-      <Categoria></Categoria>
-      <Itens></Itens>
-      <Itens></Itens>
-      <Itens></Itens>
-      <Categoria></Categoria>
-      <Itens></Itens>
-      <Itens></Itens>
-      <Itens></Itens>
+      <h2>Dados Básicos</h2>
+      <p>Nome:</p>
+      <p>Sobrenome:</p>
+      <p>Idade:</p>
     </div>
   }
 }
 
-class Categoria extends React.Component {
+class Endereco extends React.Component {
   render() {
-    return <h3>Componente Categoria</h3>
+    return <div>
+      <h2>{this.props.tipo}</h2>
+      <p>Logradouro:</p>
+      <p>CEP:</p>
+      <p>Cidade/Estado:</p>
+    </div>
   }
 }
 
-class Itens extends React.Component {
+class AreaAtuacao extends React.Component {
   render() {
-    return <h4>Componente Itens</h4>
+    return <div>
+      <h3>{this.props.title}</h3>
+      <p>{this.props.description}</p>
+    </div>
   }
 }
 
 root.render(
   <div>
-    <Pesquisa></Pesquisa>
-    <ListaProdutos></ListaProdutos>
+    <Cadastro></Cadastro>
+    <DadosBasicos></DadosBasicos>
+    <Endereco tipo='Endereço Residencial'></Endereco>
+    <Endereco tipo='Endereço Comercial'></Endereco>
+    <AreaAtuacao title='Gerente' description='Descrição xxx'></AreaAtuacao>
+    <AreaAtuacao title='Analista' description='Descrição xxx'></AreaAtuacao>
+    <AreaAtuacao title='Assistente' description='Descrição xxx'></AreaAtuacao>
   </div>
 );
 
